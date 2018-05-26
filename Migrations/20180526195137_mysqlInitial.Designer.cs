05a8131adf922640160cb88f0a23763cd3e8493e
Edgard Zadra <edgardzadra@gmail.com>
2018-05-26 18:15:50 -0300

    migracao de banco ajustes em seed e pequenas correcoes na api

--- Migrations/20180526195137_mysqlInitial.Designer.cs
+++ Migrations/20180526195137_mysqlInitial.Designer.cs
@@ -11,13 +11,14 @@ using System;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20180522182827_MessageEntity")]
-    partial class MessageEntity
+    [Migration("20180526195137_mysqlInitial")]
+    partial class mysqlInitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
+                .HasAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
                 .HasAnnotation("ProductVersion", "2.0.2-rtm-10011");
 
             modelBuilder.Entity("DatingApp.API.Models.Like", b =>
